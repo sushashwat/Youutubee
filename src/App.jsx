5ee1b92d1bb7ca44627a8429c048fbd8cf774c23
Shashwat Gupta <guptashashwat155@gmail.com>
2026-06-24 22:45:16 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// App.jsx - root component 
+// Routes, Header,Sidebar etc will be wired here
+
+function App(){
+  return(
+    <div>
+      <h1>Youtube Clone - Project Setup Done ✅</h1>
+      <p></p>
+    </div>
+  )
+}
+export default App
