2c99f10afe51710912408d9a255c8931c2f7afe3
Shashwat Gupta <guptashashwat155@gmail.com>
2026-06-25 15:31:34 +0530

    integrate Header into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-// App.jsx - root component 
-// Routes, Header,Sidebar etc will be wired here
+import Header from "./components/Header";
 
 function App(){
   return(
-    <div className="bg-yt-bg min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-yt-red">Youtube Clone - Project Setup Done ✅</h1>
-      <p></p>
+    <div className="bg-yt-bg min-h-screen">
+      <Header />
+      <main className="pt-14 p-4">
+        <p>Youtube </p>
+      </main>
     </div>
   )
 }
-export default App
+export default App;
