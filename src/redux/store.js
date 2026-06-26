3c61f500493f96103c2d32c3d399b80d2c7eb00f
Shashwat Gupta <guptashashwat155@gmail.com>
2026-06-26 22:56:07 +0530

    feat: register search reducer in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './slices/authSlice'
 import videosReducer from './slices/videosSlice'
 import channelsReducer from './slices/channelsSlice'
+import searchReducer from './slices/searchSlice'
+import { Search } from "lucide-react";
 
 export const store  = configureStore({
     reducer:{
         auth:authReducer,
         videos:videosReducer,
         channels:channelsReducer,
+        search:searchReducer,
     },
 })
