d922ae8e26c078c91792f776e8eb60d51ddb843b
Shashwat Gupta <guptashashwat155@gmail.com>
2026-06-30 16:08:11 +0530

    fix(backend): correct commentSchema variable name in Comment model

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -7,7 +7,7 @@ import mongoose from "mongoose";
  * entire video document. Linked back to its video via `video` field.
  */
 
-const schema = new mongoose.Schema(
+const commentSchema = new mongoose.Schema(
     {
         text:{type:String, required:true, trim:true},
         user:{type: mongoose.Schema.Types.ObjectId, ref:'User', required: true},
